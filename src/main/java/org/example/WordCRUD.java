f484e6b2e4b72953c5f27717b32d691969e54413
EundaChung <22200680@handong.ac.kr>
2023-09-04 19:52:43 +0900

    edit output format

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -14,7 +14,7 @@ public class WordCRUD implements ICRUD {
 
     @Override
     public Object add() {
-        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.print("\n=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();
 
@@ -28,7 +28,7 @@ public class WordCRUD implements ICRUD {
     public void addWord() {
         Word one = (Word)add();
         list.add(one);
-        System.out.print("새 단어가 단어장에 추가되었습니다 !!! \n");
+        System.out.println("\n새 단어가 단어장에 추가되었습니다 !!! \n");
 
     }
 
@@ -51,12 +51,12 @@ public class WordCRUD implements ICRUD {
     }
 
     public void listAll() {
-        System.out.println("-------------------------------- ");
+        System.out.println("\n-------------------------------- ");
         for(int i=0; i<list.size(); i++) {
             System.out.print((i+1)+" ");
             System.out.println(list.get(i).toString());
         }
-        System.out.println("-------------------------------- ");
+        System.out.println("-------------------------------- \n");
     }
 
 }
--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -11,10 +11,11 @@ public class WordManager {
     }
 
     public void start() {
+        System.out.println("*** 영단어 마스터 ***\n");
         while(true) {
             int menu = selectMenu();
             if(menu == 0) {
-                System.out.println("프로그램 종료! 다음에 만나요~");
+                System.out.println("\n프로그램 종료! 다음에 만나요~");
                 break;
             }
             if(menu == 4) {
@@ -33,7 +34,7 @@ public class WordManager {
 
     }
     public int selectMenu() {
-        System.out.print("*** 영단어 마스터 ***\n********************\n1. 모든 단어 보기\n2. 수준별 단어 보기\n3. 단어 검색\n4. 단어 추가\n5. 단어 수정\n6. 단어 삭제\n7. 파일 저장\n0.나가기\n********************\n=> 원하는 메뉴는? ");
+        System.out.print("********************\n1. 모든 단어 보기\n2. 수준별 단어 보기\n3. 단어 검색\n4. 단어 추가\n5. 단어 수정\n6. 단어 삭제\n7. 파일 저장\n0. 나가기\n********************\n=> 원하는 메뉴는? ");
 
         return s.nextInt();
     }
